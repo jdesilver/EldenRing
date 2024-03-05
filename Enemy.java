dd1bee2315857f0204cbc85f7413ba688aeae1ee
jdesilver <162068796+jdesilver@users.noreply.github.com>
2024-03-05 10:02:13 -0500

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,371 @@
+public class Enemy {
+    int health;
+    
+    public Enemy(int hlth) {
+        this.health = hlth;
+    }
+    
+    public void basic() {
+        
+    }
+    
+    public void special() {
+        
+    }
+    
+    public void ability() {
+        
+    }
+}
+
+class Margit extends Enemy {
+
+    public Margit() {
+        super(4174);
+    }
+    
+    @Override
+    public void basic() {
+
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Godrick extends Enemy {
+
+    public Godrick() {
+        super(6080);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class RedWolf extends Enemy {
+
+    public RedWolf() {
+        super(2204);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Rennala extends Enemy {
+
+    public Rennala() {
+        super(3493);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Serpent extends Enemy {
+
+    public Serpent() {
+        super(59174);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Radahn extends Enemy {
+
+    public Radahn() {
+        super(9572);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Mohg extends Enemy {
+
+    public Mohg() {
+        super(18389);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class GoldenGodfrey extends Enemy {
+
+    public GoldenGodfrey() {
+        super(21903);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Morgott extends Enemy {
+
+    public Morgott() {
+        super(10399);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class FireGiant extends Enemy {
+
+    public FireGiant() {
+        super(43263);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class GodskinDuo extends Enemy {
+
+    public GodskinDuo() {
+        super(8000);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class BeastClergyman extends Enemy {
+
+    public BeastClergyman() {
+        super(10620);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Gideon extends Enemy {
+
+    public Gideon() {
+        super(6226);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Godfrey extends Enemy {
+
+    public Godfrey() {
+        super(21903);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Radagon extends Enemy {
+
+    public Radagon() {
+        super(35339);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
+
+class Malenia extends Enemy {
+
+    public Malenia() {
+        super(33251);
+    }
+    
+    @Override
+    public void basic() {
+        
+    }
+    
+    @Override
+    public void special() {
+        
+    }
+    
+    @Override
+    public void ability() {
+        
+    }
+}
